dba1bfaa3eb2f5f0a786b9e5fed3bcd0b6a3dde5
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-27 13:44:33 +0000

    [Add]投稿機能を追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,9 +3,27 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    # 「current_user」:ヘルパーメソッドの一種。deviseをインストールすることで使えるようになる。
+    # current_userは、コードに記述するだけで、ログイン中のユーザーの情報を取得できる。
+    # つまり以下では、投稿データのuser_idを、現在ログインしているユーザーのIDに指定している。
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
   def index
   end
 
   def show
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
