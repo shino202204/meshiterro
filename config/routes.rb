90f70ca3dc21db1db08f3b46213f7b93d8a257d5
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-27 07:33:19 +0000

    [Add]Aboutページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-    #deviseのモデルを作成したことで、ルーティングが自動的に追加されている。
+    # deviseのモデルを作成したことで、ルーティングが自動的に追加されている。
     # devise_for :usersは、deviseを使用する際にURLとしてusersを含むことを示している。
     devise_for :users
+
     root to: "homes#top"
+
+    get 'homes/about' ,as: 'about'
 end
