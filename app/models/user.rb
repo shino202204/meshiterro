cc767ccbf3238001db8835b7147e346ea7732cc1
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-29 13:47:47 +0000

    [Add]usersコントローラを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   # Userモデルに対して、PostImageモデルが1:Nになるよう関連づける。
   # 1:Nの「1」側に当たるモデルに、has_manyを記載する必要がある。
   has_many :post_images, dependent: :destroy
+
+  # profile_imageという名前でActiveStorageでプロフィール画像を保存できるように設定。
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    # 「unless」：もし〜でなければ → もし画像が添付されていなければ
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width,height]).processed
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-    # get 'post_images/new'
-    # get 'post_images/index'
-    # get 'post_images/show'
-    # 上記をresourcesの記述に変更
-    # resourcesメソッド：ルーティングを一括して自動生成してくれる機能
-    # onlyオプション：生成するルーティングを限定することができる
-    # この場合、onlyの後に配列で記述されている"new","index","show"のアクション以外は、
-    # ルーティングが行われない。
-    resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
 
-    # deviseのモデルを作成したことで、ルーティングが自動的に追加されている。
-    # devise_for :usersは、deviseを使用する際にURLとしてusersを含むことを示している。
-    devise_for :users
+  # 上記をresourcesの記述に変更
+  # resourcesメソッド：ルーティングを一括して自動生成してくれる機能
+  # onlyオプション：生成するルーティングを限定することができる
+  # この場合、onlyの後に配列で記述されている"new","index","show"のアクション以外は、
+  # ルーティングが行われない。
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 
-    root to: "homes#top"
+  # deviseのモデルを作成したことで、ルーティングが自動的に追加されている。
+  # devise_for :usersは、deviseを使用する際にURLとしてusersを含むことを示している。
+  devise_for :users
 
-    get 'homes/about' ,as: 'about'
+  root to: "homes#top"
+
+  get 'homes/about' ,as: 'about'
 end
