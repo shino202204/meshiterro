d67f5e82a0a95e73ec8f40022c53567fc9b65402
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-29 14:36:06 +0000

    [Add]15章の内容を追加(50%)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    # 以下はアソシエーションを持っているモデル同士の記述方法。
+    # 特定のユーザ(@user)に関連付けられた投稿全て(.post_images)を取得し、
+    # @post_imagesに渡すという処理を行うことができる。
+    @post_images = @user.post_images
   end
 
   def edit
