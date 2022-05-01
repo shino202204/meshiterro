24d645457eb6e4a83c90d821d1abe06f9ace32eb
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-05-01 00:24:04 +0000

    [Update]コメント投稿機能を更新

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,7 +6,7 @@ class PostCommentsController < ApplicationController
     # comment = PostComment.new(post_comment_params)
     # comment.user_id = current_user.id
     comment = current_user.post_comments.new(post_comment_params)
-
+    comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
   end
